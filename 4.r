5424694627c93d621917d87a6eaf6089dc3cb746
Lijiayi111 <113895450+Lijiayi111@users.noreply.github.com>
2022-11-17 16:47:31 +0000

    Update 4.r

--- 4.r
+++ 4.r
@@ -1,6 +1,8 @@
 t80 <- 1:13 ## years since 1980
 y <- c(12,14,33,50,67,74,123,141,165,204,253,246,240) ## AIDS cases
 
+
+
 func<- function(theta,t,y) {
   ## -ve log likelihood for AIDS model y_i ~ Poi(alpha*exp(beta*t_i))
   ## theta = (alpha,beta)
@@ -21,26 +23,28 @@ hess <- function(theta,t,y) {
   alpha <- theta[1];beta <- theta[2] ## enhances readability
   ebt <- exp(beta*t) ## avoid computing twice
   H <- matrix(0,2,2) ## matrix for Hessian of -ve ll
-  71
   H[1,1] <- sum(y)/alpha^2
   H[2,2] <- alpha*sum(t^2*ebt)
   H[1,2] <- H[2,1] <- sum(t*ebt)
   H } ## hll
 
 
-fd <- th0 <- c(10,.2)
+fd <- th0 <- c(10,.5)
 grad(th0,t=t80,y=y)
 hess(th0,t=t80,y=y)
-Hessian(th0,func,y=y,t=t80,grad,eps =1e-8 )
-
+Hessian(th0,y=y,t=t80,grad = grad,eps =1e-8 )
+chol(hess(th0,t=t80,y=y))
 # object <- func(theta,...) ## ...can be used? 
 object <- func(th0,t=t80,y=y)
 gradient <- grad(th0,t=t80,y=y)
 
+#newt(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
+newt(theta= th0,func,grad,hess,t=t80,y=y,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
+
 
 #finite difference
-Hessian <- function(theta, f, y, t=t80,grad,eps){
-  eps <- 1e-8 ## finite difference interval
+Hessian <- function(theta, y,func t=t80,grad,eps){
+  ## finite difference interval
   grad0 <- grad(th0,t=t80,y=y) ## grad of nll at th0 
   Hfd <- matrix(0L, nrow = length(theta), ncol = length(theta)) ## initilize finite diference Hessian
   for (i in 1:length(th0)) { ## loop over parameters
@@ -62,7 +66,7 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
   gradient <- grad(theta,...)
   
   # Hessian calculation by finite differencing if no hessian as input.
-  if(any(is.null(hess))) hessian <- hess(theta, f,...)
+  if(any(is.null(hess))) hessian <- Hessian(theta,...)
   else hessian <- hess(theta,...) # Calling fd.Hessian defined above
   
   
@@ -70,14 +74,16 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
   if (!is.finite(object) || any(!is.finite(gradient))|| any(!is.finite(hessian)))
     stop("The objective or derivatives are not finite at the initial theta")## waring 1 
   
-
+  
   for(i in 1:maxit){
+    print(hessian)
     if(max(abs(gradient))< tol*(abs(object)+fscale)){
       cat("Convergence is done")
       
-      if (inherits(try(chol(h),silent=TRUE),"try-error")) {
+      if (inherits(try(chol(hessian),silent=TRUE),"try-error")) {
         inver_hessian = NULL
         warning("The Hessian is not positive definite at convergence")
+        
       }else{
         
         # inverse of hessian matrix. 
@@ -88,14 +94,18 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
       break
     }else{
       number_perturb <- 0
+      test <- hessian
       # Cholesky factor calculation (perturb H until this calculation is possible)
-      while(inherits(try(chol_number_perturb <- chol(number_perturb), silent = TRUE), "try-error") == TRUE) {
+      while(inherits(try(chol(test), silent = TRUE), "try-error") == TRUE) {
+        print(number_perturb)
         # Perturbing hessian to be positive definite (runs of if chol(H) fails)
-        H <- H + diag(norm(hessian)*10^number_perturb*1e-6, nrow=nrow(hessian), ncol=ncol(hessian))
+        test <- hessian+ diag(norm(hessian)*10^number_perturb*1e-6, nrow=nrow(hessian), ncol=ncol(hessian))
         number_perturb <- number_perturb + 1
+        print(hessian)
       }
+      hessian <- test 
       # Calculate forward step towards optimum (minimum)
-      Delta <- backsolve(H.chol, forwardsolve(t(H.chol), -gradient))
+      Delta <- backsolve(chol(hessian), forwardsolve(t(chol(hessian)), -gradient))
       
       
       # Ensuring steps are taken in right direction towards optimum (minimum)
@@ -110,37 +120,39 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
               after ", as.character(max.half), " halvings"))
         }
       }
-     theta <- theta + Delta
-     
-     object<-func(theta,...)
-     gradient<-grad(theta,...)
-     # Hessian calculation by finite differencing if no hessian as input.
-     if(any(is.null(hess))){
-       hessian <- Hessian(theta, f,...)
-     } else {
-       hessian <- hess(theta,...) # Calling fd.Hessian defined above
-     }
-             
+      theta <- theta + Delta
+      
+      object<-func(theta,...)
+      gradient<-grad(theta,...)
+      # Hessian calculation by finite differencing if no hessian as input.
+      if(any(is.null(hess))){
+        hessian <- Hessian(theta, f,...)
+      } else {
+        hessian <- hess(theta,...) # Calling fd.Hessian defined above
+      }
+      
     }
     
     # Checking whether convergence occurs when iter == maxit
-    if (max(abs(gradient)) < (abs(f0)+fscale)*tol){
-      cat("Convergence is done")
-      if (inherits(try(chol(h),silent=TRUE),"try-error")) {
-        inver_hessian = NULL
-        warning("The Hessian is not positive definite at convergence")
-      }else{
-        # inverse of hessian matrix. 
-        inver_hessian <- chol2inv(chol(hessian)) 
-      }
-      return(list(f=object, theta=theta, iter=maxit, g=gradient, Hi=inver_hessian)) #when converse return
-    } else {
-      warning(paste("Newton optimizer failed to converage after
-                  maxit = ", as.character(maxit), " iterations"))
-    }
+
   }
-  
+  if (max(abs(gradient)) < (abs(object)+fscale)*tol){
+    cat("Convergence is done")
+    if (inherits(try(chol(hessian),silent=TRUE),"try-error")) {
+      inver_hessian = NULL
+      warning("The Hessian is not positive definite at convergence")
+    }else{
+      # inverse of hessian matrix. 
+      inver_hessian <- chol2inv(chol(hessian)) 
+    }
+    return(list(f=object, theta=theta, iter=maxit, g=gradient, Hi=inver_hessian)) #when converse return
+  } else {
+    warning(paste("Newton optimizer failed to converage after
+                  maxit = ", as.character(maxit), " iterations"))
+  }
 }
+
+
 
 
 
