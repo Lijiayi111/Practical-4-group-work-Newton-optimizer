76bbdd5d88b9f25fecff414429a92b81b2e92be2
Lijiayi111 <113895450+Lijiayi111@users.noreply.github.com>
2022-11-17 17:59:58 +0000

    Update 4.r

--- 4.r
+++ 4.r
@@ -1,63 +1,39 @@
-t80 <- 1:13 ## years since 1980
-y <- c(12,14,33,50,67,74,123,141,165,204,253,246,240) ## AIDS cases
 
 
 
-func<- function(theta,t,y) {
-  ## -ve log likelihood for AIDS model y_i ~ Poi(alpha*exp(beta*t_i))
-  ## theta = (alpha,beta)
-  mu <- theta[1] * exp(theta[2] * t) ## mu = E(y)
-  -sum(dpois(y,mu,log=TRUE)) ## the negative log likelihood
-} ## nll
-
-grad <- function(theta,t,y) {
-  ## grad of -ve log lik of Poisson AIDS early epidemic model
-  alpha <- theta[1];beta <- theta[2] ## enhances readability
-  ebt <- exp(beta*t) ## avoid computing twice
-  -c(sum(y)/alpha - sum(ebt), ## -dl/dalpha
-     sum(y*t) - alpha*sum(t*ebt)) ## -dl/dbeta
-} ## gll
-
-hess <- function(theta,t,y) {
-  ## Hessian of -ve log lik of Poisson AIDS early epidemic model
-  alpha <- theta[1];beta <- theta[2] ## enhances readability
-  ebt <- exp(beta*t) ## avoid computing twice
-  H <- matrix(0,2,2) ## matrix for Hessian of -ve ll
-  H[1,1] <- sum(y)/alpha^2
-  H[2,2] <- alpha*sum(t^2*ebt)
-  H[1,2] <- H[2,1] <- sum(t*ebt)
-  H } ## hll
+rb <- function(th,k=2) {
+  k*(th[2]-th[1]^2)^2 + (1-th[1])^2
+}
 
 
-fd <- th0 <- c(10,.5)
-grad(th0,t=t80,y=y)
-hess(th0,t=t80,y=y)
-Hessian(th0,y=y,t=t80,grad = grad,eps =1e-8 )
-chol(hess(th0,t=t80,y=y))
-# object <- func(theta,...) ## ...can be used? 
-object <- func(th0,t=t80,y=y)
-gradient <- grad(th0,t=t80,y=y)
+gb <- function(th,k=2) {
+  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
+}
 
-#newt(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
-newt(theta= th0,func,grad,hess,t=t80,y=y,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
+
+hb <- function(th,k=2) {
+  h <- matrix(0,2,2)
+  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
+  h[2,2] <- 2*k
+  h[1,2] <- h[2,1] <- -4*k*th[1]
+  h
+}
 
 
 #finite difference
-Hessian <- function(theta, y,func t=t80,grad,eps){
+Hessian <- function(theta,gradient,eps=1e-6,...){
   ## finite difference interval
-  grad0 <- grad(th0,t=t80,y=y) ## grad of nll at th0 
+  grad0 <- grad(theta,...) ## grad of nll at th0 
   Hfd <- matrix(0L, nrow = length(theta), ncol = length(theta)) ## initilize finite diference Hessian
-  for (i in 1:length(th0)) { ## loop over parameters
-    th1 <- th0; th1[i] <- th1[i] + eps ## increase th0[i] by eps
-    grad1 <- grad(th1,t=t80,y=y) ## compute resulting nll
+  for (i in 1:length(theta)) { ## loop over parameters
+    th1 <- theta; th1[i] <- th1[i] + eps ## increase th0[i] by eps
+    grad1 <- grad(th1,...) ## compute resulting nll
     Hfd[i,] <- (grad1 - grad0)/eps ## approximate second derivs  #i, haishi ,i
     
   }
   Hfd  <- 0.5 * (t(Hfd ) + Hfd)
   return(Hfd)
 }
-
-
 
 
 
@@ -76,7 +52,7 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
   
   
   for(i in 1:maxit){
-    print(hessian)
+    #print(hessian)
     if(max(abs(gradient))< tol*(abs(object)+fscale)){
       cat("Convergence is done")
       
@@ -97,11 +73,11 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
       test <- hessian
       # Cholesky factor calculation (perturb H until this calculation is possible)
       while(inherits(try(chol(test), silent = TRUE), "try-error") == TRUE) {
-        print(number_perturb)
+
         # Perturbing hessian to be positive definite (runs of if chol(H) fails)
         test <- hessian+ diag(norm(hessian)*10^number_perturb*1e-6, nrow=nrow(hessian), ncol=ncol(hessian))
         number_perturb <- number_perturb + 1
-        print(hessian)
+        
       }
       hessian <- test 
       # Calculate forward step towards optimum (minimum)
@@ -153,6 +129,9 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
 }
 
 
+
+th <- c(1.1,10)
+newt(theta=th,func = rb, grad = gb,hess = hb, tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
 
 
 
